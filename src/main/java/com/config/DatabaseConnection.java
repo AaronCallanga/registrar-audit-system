f60a5f9bca833f0c6d7be7b9c285684a8b3be7ef
pogi <aaroncallanga01@gmail.com>
2025-08-15 19:21:09 +0800

    DatabaseConnection set up

---
+++ src/main/java/com/config/DatabaseConnection.java
@@ -0,0 +1,26 @@
+package com.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Logger;
+
+public class DatabaseConnection {
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        final String dbUrl = "jdbc:postgresql://localhost:5432/DB_NAME"; // change database
+        final String username = "postgres";
+        final String password = "admin";
+
+        try {
+            connection = DriverManager.getConnection(dbUrl, username, password);
+            System.out.println("Connection established");
+        } catch(Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return connection;
+    }
+
+}
