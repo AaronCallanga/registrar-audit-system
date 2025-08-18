177a5cd629fb42b0149350db30ed6fa1d6f3860a
pogi <aaroncallanga01@gmail.com>
2025-08-18 10:00:44 +0800

    RequiredDocument

---
+++ src/main/java/com/student/model/RequiredDocument.java
@@ -0,0 +1,24 @@
+package com.student.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class RequiredDocument {
+    private Long id;
+    private Long studentId;
+    private String documentType;
+    private String status;
+    private LocalDateTime submittedDate;
+}
