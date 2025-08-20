198b5de786b90d247df775f80bdf43acc64698e0
pogi <aaroncallanga01@gmail.com>
2025-08-20 08:00:14 +0800

    StudentDAO added methods

--- src/main/java/com/student/dao/StudentDAO.java
+++ src/main/java/com/student/dao/StudentDAO.java
@@ -3,5 +3,9 @@ package com.student.dao;
 import com.base.BaseDAO;
 import com.student.model.Student;
 
+import java.util.List;
+
 public interface StudentDAO extends BaseDAO<Student> {
+    List<Student> findAllByYearLevel(String yearLevel);
+    List<Student> findAllByProgram(String program);
 }
