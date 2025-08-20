25638f1469d117efa84ba906e57fa5c0a13857be
pogi <aaroncallanga01@gmail.com>
2025-08-20 08:23:25 +0800

    RequiredDocumentDAO added methods

--- src/main/java/com/student/dao/RequiredDocumentDAO.java
+++ src/main/java/com/student/dao/RequiredDocumentDAO.java
@@ -3,5 +3,15 @@ package com.student.dao;
 import com.base.BaseDAO;
 import com.student.model.RequiredDocument;
 
+import java.util.List;
+
 public interface RequiredDocumentDAO extends BaseDAO<RequiredDocument> {
+    //getRequiredDocumentsByStudentId
+    List<RequiredDocument> findRequiredDocumentsByStudentId(Long studentId);
+    //getRequiredDocumentsByStudentIdAndStatus - SUBMITTED / PENDING
+    List<RequiredDocument> findRequiredDocumentsByStudentIdAndStatus(Long studentId, String status);
+    //submitRequiredDocument    // change the status to SUBMITTED
+    void submitRequiredDocument(String documentType);
+
+
 }
