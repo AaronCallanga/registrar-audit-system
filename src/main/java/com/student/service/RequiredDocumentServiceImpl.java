5eca8e2fd6e8215f7c8f9dcad037d59e57e18682
pogi <aaroncallanga01@gmail.com>
2025-08-20 08:47:45 +0800

    RequiredDocumentServiceImpl - getRequiredDocumentsByStudentId

--- src/main/java/com/student/service/RequiredDocumentServiceImpl.java
+++ src/main/java/com/student/service/RequiredDocumentServiceImpl.java
@@ -1,14 +1,21 @@
 package com.student.service;
 
+import com.student.dao.RequiredDocumentDAO;
 import com.student.model.RequiredDocument;
 
 import java.util.List;
 
 public class RequiredDocumentServiceImpl implements RequiredDocumentService {
 
+    private final RequiredDocumentDAO requiredDocumentDAO;
+
+    public RequiredDocumentServiceImpl(RequiredDocumentDAO requiredDocumentDAO) {
+        this.requiredDocumentDAO = requiredDocumentDAO;
+    }
+
     @Override
     public List<RequiredDocument> getRequiredDocumentsByStudentId(Long studentId) {
-        return List.of();
+        return requiredDocumentDAO.findRequiredDocumentsByStudentId(studentId);
     }
 
     @Override
