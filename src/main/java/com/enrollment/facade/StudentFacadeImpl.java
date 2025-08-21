7b2d424ee8ef87cfd9c193474101498a0bfb39ee
pogi <aaroncallanga01@gmail.com>
2025-08-21 12:34:23 +0800

    StudentFacadeImpl set up

---
+++ src/main/java/com/enrollment/facade/StudentFacadeImpl.java
@@ -0,0 +1,38 @@
+package com.enrollment.facade;
+
+public class StudentFacadeImpl implements StudentFacade {
+    @Override
+    public void viewStudentById() {
+
+    }
+
+    @Override
+    public void removeStudentById() {
+
+    }
+
+    @Override
+    public void enrollStudent(String name, String yearLevel, String program, String contact) {
+
+    }
+
+    @Override
+    public void updateStudentInfo(Long studentId, String name, String yearLevel, String program, String contact) {
+
+    }
+
+    @Override
+    public void viewAllStudents() {
+
+    }
+
+    @Override
+    public void viewAllStudentsByYearLevel(String yearLevel) {
+
+    }
+
+    @Override
+    public void viewAllStudentsByProgram(String program) {
+
+    }
+}
