26ef854f78dfac21030ec1c30ac501b27d8d42de
pogi <aaroncallanga01@gmail.com>
2025-08-21 12:36:45 +0800

    StudentFacadeImpl - viewStudentById

--- src/main/java/com/enrollment/facade/StudentFacadeImpl.java
+++ src/main/java/com/enrollment/facade/StudentFacadeImpl.java
@@ -1,8 +1,26 @@
 package com.enrollment.facade;
 
+import com.enrollment.model.Student;
+import com.enrollment.service.StudentService;
+import com.util.UserInputUtil;
+
 public class StudentFacadeImpl implements StudentFacade {
+
+    private final StudentService studentService;
+
+    public StudentFacadeImpl(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @Override
     public void viewStudentById() {
+        Long id = UserInputUtil.getLongInput("Enter student ID: ");
+        try {
+            Student student = studentService.getStudentById(id);
+            System.out.println("Found: " + student);
+        } catch (Exception e) {
+            System.out.println("Error fetching student: " + e.getMessage());
+        }
 
     }
 
